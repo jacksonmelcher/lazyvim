451fcd631b4b1736dd4f9773024773c78b725b23
Jackson <jax@Jacksons-Mac-mini.local>
2025-12-07 11:28:02 -0500

    mini files on start

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,15 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Auto-open mini.files when opening a directory
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    local arg = vim.fn.argv(0)
+    if arg and vim.fn.isdirectory(arg) == 1 then
+      vim.defer_fn(function()
+        require("mini.files").open(arg, false)
+      end, 10)
+    end
+  end,
+})
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Disable netrw (we're using mini.files instead)
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
