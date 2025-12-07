b431283080668db05ab92696eccda0f3aa1add64
Jackson <jax@Jacksons-Mac-mini.local>
2025-12-07 11:25:27 -0500

    enabled mini.files

--- lua/plugins/mini-files.lua
+++ lua/plugins/mini-files.lua
@@ -1,82 +1,72 @@
 return {
-  "nvim-mini/mini.files",
-  opts = {
-    -- Show hidden files (dotfiles) by default
-    content = {
-      filter = nil, -- Set to nil to show all files including hidden ones
-      prefix = nil,
-      sort = nil,
-    },
-    mappings = {
-      close = "q",
-      go_in = "l",
-      go_in_plus = "L",
-      go_out = "h",
-      go_out_plus = "H",
-      mark_goto = "'",
-      mark_set = "m",
-      reset = "<BS>",
-      reveal_cwd = "@",
-      show_help = "g?",
-      synchronize = "=",
-      trim_left = "<",
-      trim_right = ">",
-    },
-    options = {
-      -- Set as the default file explorer (replaces netrw)
-      use_as_default_explorer = true,
-    },
-    windows = {
-      max_number = math.huge,
-      preview = true,
-      width_focus = 30,
-      width_nofocus = 15,
-      width_preview = 50,
-    },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    enabled = false, -- Disable default explorer
   },
-  keys = {
-    {
-      "<leader>e",
-      function()
-        require("mini.files").open(vim.api.nvim_buf_get_name(0), true)
-      end,
-      desc = "Open mini.files (Directory of Current File)",
+  {
+    "nvim-mini/mini.files",
+    opts = {
+      content = {
+        filter = nil, -- Show all files including hidden ones by default
+      },
+      options = {
+        use_as_default_explorer = true,
+      },
+      windows = {
+        preview = true,
+        width_focus = 30,
+        width_nofocus = 15,
+        width_preview = 50,
+      },
     },
-    {
-      "<leader>E",
-      function()
-        require("mini.files").open(vim.uv.cwd(), true)
-      end,
-      desc = "Open mini.files (cwd)",
+    keys = {
+      {
+        "<leader>e",
+        function()
+          if not require("mini.files").close() then
+            require("mini.files").open(LazyVim.root(), true)
+          end
+        end,
+        desc = "Toggle mini.files (Root Dir)",
+      },
+      {
+        "<leader>E",
+        function()
+          if not require("mini.files").close() then
+            require("mini.files").open(vim.uv.cwd(), true)
+          end
+        end,
+        desc = "Toggle mini.files (cwd)",
+      },
     },
+    config = function(_, opts)
+      require("mini.files").setup(opts)
+
+      -- Custom function to toggle hidden files visibility
+      local show_dotfiles = true
+
+      local filter_show = function()
+        return true
+      end
+
+      local filter_hide = function(fs_entry)
+        return not vim.startswith(fs_entry.name, ".")
+      end
+
+      local toggle_dotfiles = function()
+        show_dotfiles = not show_dotfiles
+        local new_filter = show_dotfiles and filter_show or filter_hide
+        require("mini.files").refresh({ content = { filter = new_filter } })
+      end
+
+      -- Add keymap to toggle hidden files with 'g.'
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "MiniFilesBufferCreate",
+        callback = function(args)
+          local buf_id = args.data.buf_id
+          vim.keymap.set("n", "g.", toggle_dotfiles, { buffer = buf_id, desc = "Toggle hidden files" })
+        end,
+      })
+    end,
   },
-  config = function(_, opts)
-    require("mini.files").setup(opts)
-
-    -- Custom function to toggle hidden files visibility
-    local show_dotfiles = true
-
-    local filter_show = function()
-      return true
-    end
-
-    local filter_hide = function(fs_entry)
-      return not vim.startswith(fs_entry.name, ".")
-    end
-
-    local toggle_dotfiles = function()
-      show_dotfiles = not show_dotfiles
-      local new_filter = show_dotfiles and filter_show or filter_hide
-      require("mini.files").refresh({ content = { filter = new_filter } })
-    end
-
-    -- Add keymap to toggle hidden files with 'g.'
-    vim.api.nvim_create_autocmd("User", {
-      pattern = "MiniFilesBufferCreate",
-      callback = function(args)
-        local buf_id = args.data.buf_id
-        vim.keymap.set("n", "g.", toggle_dotfiles, { buffer = buf_id, desc = "Toggle hidden files" })
-      end,
-    })
-  end,
 }
---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,9 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    -- Disable the Snacks file explorer since we're using mini.files
+    explorer = {
+      enabled = false,
+    },
+  },
+}
